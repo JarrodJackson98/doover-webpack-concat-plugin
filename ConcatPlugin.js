76ef058668316dd99e14165b80939b7f39537f6d
jj-mac-vm <jarrod@span-eng.com>
2023-05-19 08:26:17 +1000

    add remoteFile funcationality

--- ConcatPlugin.js
+++ ConcatPlugin.js
@@ -1,27 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 
-
 class ConcatenatePlugin {
   constructor(options) {
     this.source = options.source;
     this.destination = path.resolve(options.destination);
     this.name = options.name;
     this.ignore = options.ignore || [];
+    this.remoteFile = options.remoteFile;
   }
 
   apply(compiler) {
-    // Register a callback to be executed before emitting assets
     compiler.hooks.emit.tapAsync('ConcatenatePlugin', (compilation, callback) => {
-      // Check if JavaScript files exist in source directory
-      const files = fs.readdirSync(this.source)
-        .filter((file) => {
-          const fileName = path.basename(file);
-          const fileExtension = path.extname(file);
-          return fileExtension === '.js' && !this.ignore.includes(fileName);
-        });
+      const files = fs.readdirSync(this.source).filter((file) => {
+        const fileName = path.basename(file);
+        const fileExtension = path.extname(file);
+        return (
+          fileExtension === '.js' &&
+          !this.ignore.includes(fileName) &&
+          file !== this.remoteFile
+        );
+      });
 
-      // Delete files if they exist
       files.forEach((file) => {
         const filePath = path.join(this.source, file);
         fs.unlinkSync(filePath);
@@ -30,26 +30,30 @@ class ConcatenatePlugin {
       callback();
     });
 
-    // Register a callback to be executed after all assets have been emitted
     compiler.hooks.afterEmit.tapAsync('ConcatenatePlugin', (compilation, callback) => {
-      // Read JavaScript files from source directory
-      const files = fs.readdirSync(this.source)
-        .filter((file) => {
-          const fileName = path.basename(file);
-          const fileExtension = path.extname(file);
-          return fileExtension === '.js' && !this.ignore.includes(fileName);
+      const files = fs.readdirSync(this.source).filter((file) => {
+        const fileName = path.basename(file);
+        const fileExtension = path.extname(file);
+        return (
+          fileExtension === '.js' &&
+          !this.ignore.includes(fileName) &&
+          file !== this.remoteFile
+        );
+      }).sort();
+
+      const contents = files
+        .map((file) => {
+          const filePath = path.join(this.source, file);
+          return fs.readFileSync(filePath, 'utf8');
         })
-        .sort(); // Sort files alphabetically
+        .join('');
 
-      // Concatenate contents of all files
-      const contents = files.map((file) => {
-        const filePath = path.join(this.source, file);
-        return fs.readFileSync(filePath, 'utf8');
-      }).join('');
-
-      // Write concatenated output to destination directory
       const outputPath = path.join(this.destination, this.name);
       fs.writeFileSync(outputPath, contents);
+
+      const remoteFilePath = path.join(this.source, this.remoteFile);
+      const remoteFileDestination = path.join(this.destination, this.remoteFile);
+      fs.copyFileSync(remoteFilePath, remoteFileDestination);
 
       callback();
     });
